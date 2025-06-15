746720f8b27e6213055b61b0cac81a0dff42e83c
BRHELPDESK <haneefbayar3@gmail.com>
2025-06-15 07:23:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.querySelectorAll("button")[0].onclick = function () {
-  alert("Registered successfully!");
-};
-document.querySelectorAll("button")[1].onclick = function () {
-  alert("Logged in!");
-};
+function register() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(user => alert("Registered Successfully"))
+    .catch(error => alert(error.message));
+}
+function login() {
+  var email = document.getElementById("loginEmail").value;
+  var password = document.getElementById("loginPassword").value;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => alert("Login Success"))
+    .catch(error => alert(error.message));
+}
