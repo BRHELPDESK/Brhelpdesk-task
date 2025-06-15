d1b46c73daaea2a437540ed86681b92621a7cf09
BRHELPDESK <haneefbayar3@gmail.com>
2025-06-15 07:21:28 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDRkHfkz-kGRN6OgWmq2Hi9lqYENP6GJbg",
+  authDomain: "brhelpdesk-6a014.firebaseapp.com",
+  databaseURL: "https://brhelpdesk-6a014-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "brhelpdesk-6a014",
+  storageBucket: "brhelpdesk-6a014.appspot.com",
+  messagingSenderId: "559099521764",
+  appId: "1:559099521764:web:34871192eb8085f89f1af1",
+  measurementId: "G-NTBKQW42TY"
+};
+firebase.initializeApp(firebaseConfig);
