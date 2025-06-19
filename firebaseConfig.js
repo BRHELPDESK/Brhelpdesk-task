3a8b6d394393cbda96924d59152fc0eed3d55d39
BRHELPDESK <haneefbayar3@gmail.com>
2025-06-19 06:26:00 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -17,3 +17,19 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRkHfkz-kGRN6OgWmq2Hi9lqYENP6GJbg",
+  authDomain: "brhelpdesk-6a014.firebaseapp.com",
+  databaseURL: "https://brhelpdesk-6a014-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "brhelpdesk-6a014",
+  storageBucket: "brhelpdesk-6a014.appspot.com",
+  messagingSenderId: "559099521764",
+  appId: "1:559099521764:web:34871192eb8085f89f1af1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+window.db = db;
